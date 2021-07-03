13df3cb6dd5fd26c664d5b3187a48b119734ecfe
Rajath S Rao <sraorajath@gmail.com>
2021-07-03 12:23:55 +0530

    folder structure setup

---
+++ src/app/views/views-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ViewsComponent } from './views.component';
+
+const routes: Routes = [
+  {
+    path: '', component: ViewsComponent,
+    children: [
+      {
+        path: '', redirectTo: '/dashboard', pathMatch: 'full'
+      },
+      {
+        path: 'dashboard', component: DashboardComponent
+      },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+
+export class ViewsRouteModule {}
