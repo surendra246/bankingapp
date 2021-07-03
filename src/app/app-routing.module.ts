6e32bd183cc6cdd4e6cff70d6947cd1227bc415b
Surendra Kumar <surendra.kumar@zazmic.com>
2021-07-03 17:15:56 +0530

    modified the routes for payment history

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaymentHistoryComponent } from './views/payment-history/payment-history.component';
 
+//define the Route
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./views/views.module').then(m => m.ViewsModule)
-  }
+  },
+  {
+    path: 'transaction/:id',
+    component:PaymentHistoryComponent
+  },
+  //adding lazy loading proccess
+  {path:'transaction/:id',loadChildren:()=>import('./views/payment-history/payment-history.module').then(m=>m.PaymentHistoryModule)}
+
 ];
 
 @NgModule({
