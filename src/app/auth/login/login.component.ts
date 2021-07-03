07147b40d0f2ec30d81d9026f830c31861bdae10
Shaik Rahamthulla <reallyshaik@gmail.com>
2021-07-03 16:34:27 +0530

    Login form: Basic login form : authorization

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,21 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ServicesService } from '../../core/service.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  // service instances are created in constuructor
+  constructor(
+    private servicesService: ServicesService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  // model object which maintains form details
   model: any = {};
 
+  // on submit this logic check user credentials are autorized or not
   onSubmit() {
-    alert('SUCCESS!! :-)\n\n' )
+    this.servicesService.getAllCustomers().subscribe((data) => {
+      for (let obj of data) {
+        if (obj.customer_id.localeCompare(this.model.customerid) == -1) {
+          alert('Please enter valid Credentials');
+          break;
+        } else if (obj.customer_id.localeCompare(this.model.customerid) == 0) {
+          let myurl = '/dashboard';
+          this.router.navigateByUrl(myurl).then((e) => {
+            if (e) {
+              console.log('Navigation is successful!');
+            } else {
+              console.log('Navigation has failed!');
+            }
+          });
+        }
+      }
+    });
   }
-
 }
