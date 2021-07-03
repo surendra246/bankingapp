53c8ee7c093ff1c82381dacb9a38e3fa5940f96c
Rajath S Rao <sraorajath@gmail.com>
2021-07-03 15:22:20 +0530

    added few comments

--- src/app/views/views-routing.module.ts
+++ src/app/views/views-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   {
     path: '', component: ViewsComponent,
     children: [
+      // Route Configuration for dashboard
       {
         path: '', redirectTo: '/dashboard', pathMatch: 'full'
       },
